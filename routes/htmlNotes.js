d6ac65937b702f7b02e97c17d5425d63333fb900
Joshua Piper <joshua.g.piper@gmail.com>
2020-08-01 14:45:05 +1000

    gave html pages a folder for better structuring

--- routes/htmlNotes.js
+++ routes/htmlNotes.js
@@ -2,10 +2,10 @@ const path = require("path");
 
 module.exports = (app) => {
   app.get("/notes", (req, res) =>
-    res.sendfile(path.join(__dirname, "../views/notes.html"))
+    res.sendfile(path.join(__dirname, "../public/notes.html"))
   );
 
   app.get("/", (req, res) =>
-    res.sendFile(path.join(__dirname, "../views/index.html"))
+    res.sendFile(path.join(__dirname, "../public/index.html"))
   );
 };
