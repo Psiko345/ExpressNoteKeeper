9b8ccb64628ed8f890b8b7bf53e262bbfd47b28e
Joshua Piper <joshua.g.piper@gmail.com>
2020-08-01 15:21:33 +1000

    fixed routes.

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 // requirements
 const express = require("express");
+const path = require("path");
+const dbExport = require("./db/db.json");
 
 // config
 const app = express();
@@ -8,10 +10,21 @@ const PORT = process.env.PORT || 8080;
 // data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static(path.join(__dirname + "/public")));
 
 // routes
-require("./routes/htmlNotes")(app);
-require("/routes/apiNotes")(app);
+//require("/routes/htmlNotes")(app);
+//require("/routes/apiNotes")(app);
+
+app.get("/notes", (req, res) =>
+  res.sendFile(path.join(__dirname, "./webpages/notes.html"))
+);
+
+app.get("/", (req, res) =>
+  res.sendFile(path.join(__dirname, "./webpages/index.html"))
+);
+
+app.get("/api/notes", (req, res) => res.json(dbExport));
 
 // listening
 app.listen(PORT, () => {
