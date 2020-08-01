a8a4825a2dbea59a71ab7a3464e51533c2f79be3
Joshua Piper <joshua.g.piper@gmail.com>
2020-08-01 14:46:15 +1000

    updated path in htmlNotes.

--- routes/htmlNotes.js
+++ routes/htmlNotes.js
@@ -2,10 +2,10 @@ const path = require("path");
 
 module.exports = (app) => {
   app.get("/notes", (req, res) =>
-    res.sendfile(path.join(__dirname, "../public/notes.html"))
+    res.sendfile(path.join(__dirname, "../webpages/notes.html"))
   );
 
   app.get("/", (req, res) =>
-    res.sendFile(path.join(__dirname, "../public/index.html"))
+    res.sendFile(path.join(__dirname, "../webpages/index.html"))
   );
 };
